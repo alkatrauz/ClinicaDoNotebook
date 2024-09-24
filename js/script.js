c481df1a8a9ff8ece295d303f09aaf11738d7c68
alkatrauz <163009399+alkatrauz@users.noreply.github.com>
2024-09-24 17:07:55 -0300

    main

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerMenu = document.getElementById('hamburger-menu');
-const navbar = document.getElementById('navbar');
-
-hamburgerMenu.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-});
